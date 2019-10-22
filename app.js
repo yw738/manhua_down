94e84746c5510e0bddc3e8b50752919f06067805
杨伟 <yangw@kkou.cn>
2019-10-23 00:17:51 +0800

    app

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 
 const RNRoute = require('./routes/RNRoute');
-const fzdmRoute = require('./routes/fzdmRoute');//风之漫画
+const fzdmRoute = require('./routes/fzdmRoute'); //风之漫画
 const mhApi = require('./routes/72mhRoute');
 const api = require("./routes/ApiRoute");
 app.use(RNRoute);
 app.use(fzdmRoute);
 app.use(mhApi);
 app.use(api);
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 app.listen(80, function() {
     console.log('启动');
 });
